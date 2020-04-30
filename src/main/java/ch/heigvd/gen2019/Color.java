cf48107a6c084c9d18a1c8b9992280608985f9d7
Simon <simon.mattei@heig-vd.ch>
2020-04-30 11:49:35 +0200

    OrdersWriter.getColorfor : Replaced Conditional by polymorphism. Product - color is now Color

--- src/main/java/ch/heigvd/gen2019/Color.java
+++ src/main/java/ch/heigvd/gen2019/Color.java
@@ -9,33 +9,29 @@
  Etudiants : Mattei Simon
  */
 
-
 package ch.heigvd.gen2019;
 
-import java.util.ArrayList;
-import java.util.List;
+public abstract class Color {
+    public abstract String toString();
+}
 
-public class Order {
-    private List<Product> products = new ArrayList<Product>();
-    private int id;
-
-    public Order(int id) {
-        this.id = id;
+class Red extends Color {
+    @Override
+    public String toString() {
+        return "red";
     }
+}
 
-    public int getOrderId() {
-        return id;
+class Blue extends Color {
+    @Override
+    public String toString() {
+        return "blue";
     }
+}
 
-    public int getProductsCount() {
-        return products.size();
-    }
-
-    public Product getProduct(int j) {
-        return products.get(j);
-    }
-
-    public void AddProduct(Product product) {
-        products.add(product);
+class Yellow extends Color {
+    @Override
+    public String toString() {
+        return "yellow";
     }
 }
--- src/main/java/ch/heigvd/gen2019/Product.java
+++ src/main/java/ch/heigvd/gen2019/Product.java
@@ -1,26 +1,26 @@
 /*
-  _______    _______  _____  ___       ___            __       _______     ______    _______
- /" _   "|  /"     "|(\"   \|"  \     |"  |          /""\     |   _  "\   /    " \  /" __   )
-(: ( \___) (: ______)|.\\   \    |    ||  |         /    \    (. |_)  :) // ____  \(__/ _) ./
- \/ \       \/    |  |: \.   \\  |    |:  |        /' /\  \   |:     \/ /  /    ) :)   /  //
- //  \ ___  // ___)_ |.  \    \. |     \  |___    //  __'  \  (|  _  \\(: (____/ // __ \_ \\
-(:   _(  _|(:      "||    \    \ |    ( \_|:  \  /   /  \\  \ |: |_)  :)\        / (: \__) :\
- \_______)  \_______) \___|\____\)     \_______)(___/    \___)(_______/  \"_____/   \_______)
-
+  _______    _______  _____  ___       ___            __       _______     ______   ___  ___
+ /" _   "|  /"     "|(\"   \|"  \     |"  |          /""\     |   _  "\   /    " \ (: "||_  |
+(: ( \___) (: ______)|.\\   \    |    ||  |         /    \    (. |_)  :) // ____  \|  (__) :|
+ \/ \       \/    |  |: \.   \\  |    |:  |        /' /\  \   |:     \/ /  /    ) :)\____  ||
+ //  \ ___  // ___)_ |.  \    \. |     \  |___    //  __'  \  (|  _  \\(: (____/ //     _\ '|
+(:   _(  _|(:      "||    \    \ |    ( \_|:  \  /   /  \\  \ |: |_)  :)\        /     /" \_|\
+ \_______)  \_______) \___|\____\)     \_______)(___/    \___)(_______/  \"_____/     (_______)
  Etudiants : Mattei Simon
  */
+
 
 package ch.heigvd.gen2019;
 
 public class Product {
     public static final int SIZE_NOT_APPLICABLE = -1;
     private String code;
-    private int color;
+    private Color color;
     private int size;
     private double price;
     private String currency;
 
-    public Product(String code, int color, int size, double price, String currency) {
+    public Product(String code, Color color, int size, double price, String currency) {
         this.code = code;
         this.color = color;
         this.size = size;
@@ -32,7 +32,7 @@ public class Product {
         return code;
     }
 
-    public int getColor() {
+    public Color getColor() {
         return color;
     }
 
