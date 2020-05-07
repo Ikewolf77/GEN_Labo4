14f4871854ad9f1bf99a91fb5dbb1ed890a67943
Simon <simon.mattei@heig-vd.ch>
2020-05-07 10:05:56 +0200

    bad smell : temprorary variable; refactor : replace temp with query for orders

--- src/main/java/ch/heigvd/gen2019/OrdersWriter.java
+++ src/main/java/ch/heigvd/gen2019/OrdersWriter.java
@@ -22,8 +22,7 @@ public class OrdersWriter {
         StringBuffer sb = new StringBuffer("{\"orders\": [");
 
         for (int i = 0; i < orders.getOrdersCount(); i++) {
-            Order order = orders.getOrder(i);
-            getOrderContents(sb, order);
+            getOrderContents(sb, orders.getOrder(i));
         }
 
         if (orders.getOrdersCount() > 0) {
