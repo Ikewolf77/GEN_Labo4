f93b1bcd9e047aa572d89cef5d710dc75cf74a33
Simon <simon.mattei@heig-vd.ch>
2020-05-07 09:27:16 +0200

    bad smell : temp variable; refactor : inline query

--- src/main/java/ch/heigvd/gen2019/OrdersWriter.java
+++ src/main/java/ch/heigvd/gen2019/OrdersWriter.java
@@ -31,9 +31,7 @@ public class OrdersWriter {
             sb.append(", ");
             sb.append("\"products\": [");
             for (int j = 0; j < order.getProductsCount(); j++) {
-                Product product = order.getProduct(j);
-
-                getProductContents(sb, product);
+                getProductContents(sb, order.getProduct(j));
             }
 
             if (order.getProductsCount() > 0) {
